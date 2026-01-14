b7ff49458f814df776174a5abb6d105359aee408
Huy Thai <KennyT995@gmail.com>
2026-01-14 21:31:58 +0000

    Try fixing this error: `Runtime ReferenceError: DialogDescription is not

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -17,6 +17,7 @@ import {
   DialogContent,
   DialogHeader,
   DialogTitle,
+  DialogDescription,
   DialogTrigger,
 } from '@/components/ui/dialog';
 import { VisionForm } from '@/components/vision-form';
