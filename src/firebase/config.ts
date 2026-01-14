91902fd6ea54c1696a9b47d50fe256fc5d6bd88e
Huy Thai <KennyT995@gmail.com>
2026-01-14 13:20:19 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4769439297-fbd83",
+  "appId": "1:527333325823:web:b892a689b9e702a31645ca",
+  "apiKey": "AIzaSyDdiNmwNFcxf9py-0xXg8CiJf5P6CsleNM",
+  "authDomain": "studio-4769439297-fbd83.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "527333325823"
+};
